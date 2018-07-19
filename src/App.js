b882670e747e1f041c79578f3dc898493cfb7b05
917261519 <shruti12@sfsu.edu>
2018-07-18 19:33:21 -0700

    login component with react only

--- src/App.js
+++ src/App.js
@@ -2,17 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Login from './components/Login.js'
+
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state =  {
+            loginSuccess: false,
+            username: ""
+        }
+        this.handleLogin = this.handleLogin.bind(this);
+    }
+
+    handleLogin(username, password) {
+        // simulating ajax call
+        if(username == 'shrutiB' && password == 'shruti') {
+            this.setState({
+                loginSuccess: true,
+                username: username
+            })
+        }
+    }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+          </header>
+
+          { (!this.state.loginSuccess )? <Login handleLogin={this.handleLogin}></Login> : <p> you are logged in {this.state.username}</p> }
+          
       </div>
     );
   }
